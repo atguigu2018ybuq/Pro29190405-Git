1f0be4d2f777f01718c6e4365115490dcfdf5358
peter <peter@atguigu.com>
2019-05-22 21:22:30 +0800

    测试分支

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("hello git .......");
 		System.out.println("hello git .......");
 		System.out.println("hello git .......");
-		System.out.println("hello git .......");
+		System.out.println("hello git .......");//edit by hotfix
 		System.out.println("hello git .......");//edit
 		System.out.println("hello git .......");
 		System.out.println("hello git .......");//edit
